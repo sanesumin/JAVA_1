26db02f904903b9f4d9a24bed8d109e5d7a07273
1-3 <1-3@1-3-PC>
2018-08-09 16:38:15 +0900

    이클립스 설치와 자바소스 분석

---
+++ 이클립스 설치와 자바소스 분석/src/run/stmt/ex/RunEx.java
@@ -0,0 +1,14 @@
+package run.stmt.ex;
+
+public class RunEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1; //변수 x를 선언하고 1을 저장
+		int y = 2; //변수 y를 선언하고 2를 저장
+		int result = x + y;//x와 y를 더함
+		System.out.println(x+"+"+y+"="+ result);
+		//콘솔에 출력하는 메소드 호출
+	}
+
+}
