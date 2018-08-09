b78677805f7398bffd59a4fc652325b99ec182c3
1-3 <1-3@1-3-PC>
2018-08-09 16:37:54 +0900

    실습

---
+++ 실습/src/ex1/ex1.java
@@ -0,0 +1,13 @@
+package ex1;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		
+		int score1 = 100, score2 = 85;
+		System.out.println(score1);
+		System.out.println(score2);
+
+	}
+
+}
